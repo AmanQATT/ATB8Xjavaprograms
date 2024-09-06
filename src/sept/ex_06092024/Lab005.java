0c52427987844c3fbf4b526c1b117795c9e1df19
AmanQATT <princeaman8153@gmail.com>
2024-09-07 00:18:16 +0530

    Add the files

---
+++ src/sept/ex_06092024/Lab005.java
@@ -0,0 +1,11 @@
+package sept.ex_06092024;
+
+public class Lab005 {
+    public static void main(String[] args) {
+        //this code will not be executed.
+        //this is used to show the user or developer instructions.
+        //you can write anything here
+
+       System.out.println("Hello world!");
+    }
+}
---
+++ src/sept/ex_06092024/Lab006.java
@@ -0,0 +1,14 @@
+//below code is package : folder with sept
+package sept.ex_06092024;
+/*author - aman
+*program - ATB8X
+* Program to explain comments
+ */
+//program java with the class file,name of the class is Lab006
+public class Lab006 {
+    //This is main function, JVM starts from here
+    public static void main(String[] args) {
+        //Below code will execute, it will print hello world
+        System.out.println("Hello world!");
+    }
+}
