014f4d3b4e6df10197ad9a39e3e4a3c49b22b26b
AmanQATT <princeaman8153@gmail.com>
2024-09-07 00:36:51 +0530

    Add the files

---
+++ src/sept/ex_06092024/Lab007.java
@@ -0,0 +1,15 @@
+package sept.ex_06092024;
+
+public class Lab007 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+    }
+
+    public static void main(String args) {
+        System.out.println("Hello world!");
+    }
+        public void main(int args) {
+            System.out.println("Hello world!");
+    }
+}
---
+++ src/sept/ex_06092024/Lab008.java
@@ -0,0 +1,7 @@
+package sept.ex_06092024;
+
+public class Lab008 {
+    public static void main(String[] nilesh) {
+        System.out.println("Hello world!");
+    }
+}
