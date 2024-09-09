04b5ccc1028de8c4cfce772210f42c8d27df7900
AmanQATT <princeaman8153@gmail.com>
2024-09-09 20:49:44 +0530

    Add the files

---
+++ src/sept/ex_09092024/Lab009.java
@@ -0,0 +1,16 @@
+package sept.ex_09092024;
+
+public class Lab009 {
+    public static void main(String[] args) {
+        //java starts from here
+        //keywords and identifiers
+        //simple words
+        //simple english --> pre-defined meaning in java programs.
+        //they are in blue or kesari colour in intelliJ
+        //package , public , class ,static,void -> keywords
+        //they will always in lower case
+
+
+
+    }
+}
---
+++ src/sept/ex_09092024/Lab011.java
@@ -0,0 +1,9 @@
+package sept.ex_09092024;
+
+public class Lab011 {
+    public static void main(String[] args) {
+        //variables , datatypes , identifiers
+        boolean is_mohit_married = true;
+        boolean you_have_audi = false;
+    }
+}
