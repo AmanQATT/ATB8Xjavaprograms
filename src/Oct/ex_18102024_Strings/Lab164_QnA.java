4705f755d45b3016bdd4f9aa2a916f86a5badf79
AmanQATT <princeaman8153@gmail.com>
2024-10-23 16:56:00 +0530

    All the files 18Oct2024

--- src/Oct/ex_18102024_Strings/Lab164_QnA.java
+++ src/Oct/ex_18102024_Strings/Lab164_QnA.java
@@ -3,10 +3,15 @@ package Oct.ex_18102024_Strings;
 public class Lab164_QnA {
     public static void main(String[] args) {
 
+
         String str1 = "Hello";
-        String str2 = str1.concat(" World");
-        System.out.println(str1); // Output: Hello
-        System.out.println(str2); // Output: Hello World
+        str1.concat(" World");
+        System.out.println(str1); // Hello
+        // There will be a new String Hello Wolrd also(in SCP)
+        // str1 = str1.concat(" World"); - Hello World
+
+
+
 
     }
 }
